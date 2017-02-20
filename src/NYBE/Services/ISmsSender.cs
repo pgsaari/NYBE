d0253dc88d640a2c2c7ffcf5428942d2a65f85df
Paul <pgsaari@uwm.edu>
2017-02-19 22:12:34 -0600

    Initial Commit of Project: Hello World!

---
+++ src/NYBE/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NYBE.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
